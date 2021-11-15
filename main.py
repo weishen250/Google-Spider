270d38603519edaf08ed270004a674fb29c6295c
weishen250 <71580418+weishen250@users.noreply.github.com>
2021-11-15 20:26:11 +0800

    Add files via upload

--- main.py
+++ main.py
@@ -4,6 +4,12 @@ import ReRuls
 import ReadTitile
 
 global query
+
+isExists=os.path.exists('result')
+if not isExists:
+	os.makedirs('result') 
+
+
 query = input("请输入需要搜索的谷歌命令：")
 
 page = int(input("请输入需要爬行的页数："))
